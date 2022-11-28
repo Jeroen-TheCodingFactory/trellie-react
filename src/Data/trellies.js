297162a66120d1b3bad6c0ab594b1b6bd459d344
Jeroen Rijsdijk <88040406+Jeroen-TheCodingFactory@users.noreply.github.com>
2022-11-28 16:53:05 +0100

    made first version (#1)

---
+++ src/Data/trellies.js
@@ -0,0 +1,111 @@
+const data = [
+    {
+        id: 1,
+        title: "Video's",
+        activities: [
+            {
+                id: 1,
+                label: "Vandaag",
+                description: "Les 3 opnemen"
+            },
+            {
+                id: 2,
+                label: "Vandaag",
+                description: "Les 4 opnemen"
+            },
+            {
+                id: 3,
+                label: "Vandaag",
+                description: "Les 3 & les 4 uploaden"
+            },
+            {
+                id: 4,
+                label: "Vandaag",
+                description: "Teamsbericht over les 3 en 4 verzenden"
+            },
+        ]
+    },
+    {
+        id: 1,
+        title: "VUE films die ik ga zien",
+        activities: [
+            {
+                id: 1,
+                label: "Ooit",
+                description: "The Estate"
+            },
+            {
+                id: 2,
+                label: "Vandaag",
+                description: "The Menu"
+            },
+            {
+                id: 3,
+                label: "Vandaag",
+                description: "Violent Night"
+            },
+        ]
+    },
+    {
+        id: 1,
+        title: "Games Backlog",
+        activities: [
+            {
+                id: 1,
+                label: "Weekend",
+                description: "The Estate"
+            },
+            {
+                id: 2,
+                label: "Weekend",
+                description: "Dante's Inferno"
+            },
+            {
+                id: 3,
+                label: "Ooit",
+                description: "God of War"
+            },
+            {
+                id: 4,
+                label: "Ooit",
+                description: "God of War II"
+            },
+            {
+                id: 5,
+                label: "Ooit",
+                description: "God of War III"
+            },
+            {
+                id: 6,
+                label: "Ooit",
+                description: "God of War Chains of Olympus"
+            },
+            {
+                id: 7,
+                label: "Ooit",
+                description: "Ghost of Sparta"
+            },
+            {
+                id: 8,
+                label: "Ooit",
+                description: "God of War Ascension"
+            },
+            {
+                id: 9,
+                label: "Ooit",
+                description: "God of War (2018)"
+            },
+            {
+                id: 10,
+                label: "Ooit",
+                description: "God of War Ragnarok"
+            },
+        ]
+    },
+    
+
+
+
+];
+
+export default data;
